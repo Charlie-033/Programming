18a7630698be27e45f339a19ca2f505b45d98782
Charlie-033 <ahmedeleus1@gmail.com>
2024-10-15 15:55:09 +0000

    Conversion of datatypes

---
+++ 01_basics/03.ConversionOperation.js
@@ -0,0 +1,26 @@
+let score = "Eleus"
+
+//console.log(typeof score)
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+// "33" = 33
+// "33abc" = NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = "Eleus"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log( booleanIsLoggedIn)
+
+// 1 => true , 0 => false , "Eleus" => true , "" => false
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
