0265e486504b6b4c30a253fe9ba5d250096467e9
Charlie-033 <ahmedeleus1@gmail.com>
2024-10-15 13:57:20 +0000

    datatypes

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,18 @@
+const accountId = 14433
+let accountEmail = "eleus@gmail.com"
+var accountpass = "1122"
+accountCity = "Rajshahi"
+let accountState; 
+
+// accountId = 22 // not allowed
+
+
+
+accountEmail = "acer@gmail.com"
+accountpass = "8899"
+accountCity = "Dhaka"
+console.log(accountId);
+
+
+console.table([ accountEmail, accountId ,accountpass,accountCity, accountState])
+
