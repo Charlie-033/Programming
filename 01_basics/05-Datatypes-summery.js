31554bb01867c32ca6fa6d322a94e2505d893b1f
Charlie-033 <ahmedeleus1@gmail.com>
2024-10-16 15:39:19 +0000

    Datatypes

---
+++ 01_basics/05-Datatypes-summery.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// 7 Types : String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+const score = 100
+const scorevalue = 100.2
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let Username;
+
+const id = Symbol('123')
+const atotherId = Symbol('123')
+
+console.log(id === atotherId);
+
+const bigNumber = 1365463265498n
+
+// Reference (Non-Primitive)
+
+// Array , Objects , Functions
+
+
+
+
+
+
+
+const heros = ["Ironman", "Captain america", "Spiderman"]             
+
+let MyObject 
+    name = " Eleus"
+    age = 21  
+
+const MyFunctions=function(){
+    console.log("Hello World")};
+    
+
+console.log(typeof id);
+
+
+
+
