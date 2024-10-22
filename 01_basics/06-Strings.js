1739f77bcc146bae8c3aee37604e86f9791fc578
Charlie-033 <ahmedeleus1@gmail.com>
2024-10-22 13:49:50 +0000

    strings

---
+++ 01_basics/06-Strings.js
@@ -0,0 +1,36 @@
+const name = "Eleus"
+const repoCount = 100
+
+//console.log(name+repoCount+"value");
+
+//console.log(`Hello my name is ${name}and my repo count is ${repoCount}`);
+
+const GameName = new String('Freefire')
+//console.log(GameName[0]);
+//console.log(GameName[2]);
+//console.log(GameName.__proto__);
+//console.log(GameName.length);
+//console.log(GameName.toLocaleUpperCase());
+//console.log(GameName.charAt(1));
+
+//console.log(GameName.indexOf('r'));
+//console.log(GameName.substring(0, 4));
+console.log(GameName.slice(-4, 8));
+console.log(GameName.split('-'));
+
+
+const newString ="    Eleus    "
+//console.log(newString);
+//console.log(newString.trim());
+
+const url = "https://eleus.com/eleus%21ahmed"
+//console.log(url.replace('%','-' ));
+//console.log(url.includes('eleus'));
+
+console.log(url.includes('Fcuk'));
+
+
+
+
+
+
