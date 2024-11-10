4393891554d21ca86058dccacf160c6e55bcca9d
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-10 14:02:36 +0000

    math and nums

---
+++ 01_basics/07-nums_math.js
@@ -0,0 +1,39 @@
+const score = 400
+
+const balance = new Number(101)
+//console.log(balance);
+
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+const anotherNum = 44.899
+//console.log(anotherNum.toPrecision(3));
+//console.log(anotherNum.toPrecision(4));
+
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-BN'));
+
+//***************************Maths********************/
+
+//console.log(Math);
+//console.log(Math.abs(-4));
+//console.log(Math.round(9.3));
+//console.log(Math.ceil(9.3));
+//console.log(Math.floor(9.3));
+//console.log(Math.min(9,3));
+//console.log(Math.max(9,3));
+
+
+console.log(Math.random());
+//console.log((Math.random()*10) + 1);
+//console.log((Math.random()*10) + 1);
+
+//console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 11
+const max = 22
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+console.log(Math.floor(Math.random() * (max - min + 1)) + max)
