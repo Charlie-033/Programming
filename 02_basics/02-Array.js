0013aca75049d62ec29659616f2398b24cd1120d
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-11 05:27:19 +0000

    Array2

---
+++ 02_basics/02-Array.js
@@ -0,0 +1,37 @@
+const marvel_heroes = ["Thor", "Ironman", "Spiderman"]
+const dc_heroes = ["Superman", "Flash", "Batman"]
+
+//marvel_heroes.push(dc_heroes)
+//console.log(marvel_heroes);
+
+
+const allheroes = marvel_heroes.concat(dc_heroes)
+//console.log(allheroes);
+
+const All_heroes = [...marvel_heroes, ...dc_heroes]
+//console.log(All_heroes);
+
+//const Array = [1, 2, 3, [8, 9], 7,[6, 3,[0, 2], 5]]
+//const realArray = Array.flat(Infinity)
+//console.log(realArray);
+
+
+//console.log(Array.isArray("Eleus"));
+//console.log(Array.from('Eleus'));
+//console.log(Array.from({name:'Eleus'}));
+
+let score1 = 101
+let score2 = 201
+let score3 = 301
+
+
+console.log(Array.of(score1,score2,score3));
+console.log(Array.of(score1,score2));
+
+
+ 
+
+
+
+
+
