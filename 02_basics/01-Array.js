77c57b98538db753f9531c23c82dfcabb1994150
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-11 04:59:18 +0000

    Array1

---
+++ 02_basics/01-Array.js
@@ -0,0 +1,55 @@
+const myArray = [0, 1, 2, 3, 4, 5]
+
+//console.log(myArray[1]);
+
+
+const myHeros = ["Ironman", "Batman"]
+
+//console.log(myHeros);
+
+const myArr2 = [1, 2, 3, 4]
+//console.log(myArr2[0]);
+
+
+// Array Methods//
+
+//myArray.push(4)
+//myArray.push(44)
+//myArray.pop()   //Remove the last digit//
+
+
+//myArray.unshift(8)    //Replace the value at 1st digit..
+//myArray.shift()      //Remove the 1st value...
+
+
+
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(9));
+//console.log(myArray.indexOf(1));
+
+
+const newArr = myArray.join()   //Converts to string
+//console.log(newArr);
+//console.log(typeof newArr);
+
+//slice/ splice
+
+console.log("A", myArray);
+
+const myn1 = myArray.slice(0, 2)
+console.log(myn1);
+console.log("B", myArray);
+
+const myn2 = myArray.splice(0, 2)
+console.log("C", myArray);
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
