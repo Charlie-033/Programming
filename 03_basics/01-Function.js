bb0bc839121f824184846810245a6948465e7e72
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-16 05:03:57 +0000

    Function

--- 03_basics/01-Function.js
+++ 03_basics/01-Function.js
@@ -7,4 +7,46 @@ console.log("S");
     
 }
 
-name()
+//name()
+
+//function addTwonumber(num1, num2) {
+   //console.log(num1 + num2);
+    
+    
+//}
+
+
+function addTwonumber(num1, num2) {
+   //let result = num1 + num2
+
+   //return result
+   return num1 + num2
+    
+    
+}
+
+//addTwonumber(3, 6)
+//addTwonumber(3, "6")
+//addTwonumber(3, "a")
+//addTwonumber(3, null)
+
+
+const result = addTwonumber(3, 5)
+
+//console.log( "Result" , result);
+
+
+function loginuserMassege(username = "Bot") {
+    if (!username) {
+        console.log("Please enter a username");
+        return
+        
+    }
+    return `${username} Just logged in`
+    
+}
+
+// console.log(loginuserMassege("Eleus"))
+console.log(loginuserMassege("Eleus"))
+
+
