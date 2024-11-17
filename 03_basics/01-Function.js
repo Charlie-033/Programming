e4677237be58c3df106cfa9fc2d2c18f804948bf
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-17 06:22:37 +0000

    Functions

--- 03_basics/01-Function.js
+++ 03_basics/01-Function.js
@@ -47,6 +47,36 @@ function loginuserMassege(username = "Bot") {
 }
 
 // console.log(loginuserMassege("Eleus"))
-console.log(loginuserMassege("Eleus"))
+//console.log(loginuserMassege("Eleus"))
 
+
+function calculatecartprice(val1, val2,...number1) {
+    return number1
+    }
+
+console.log(calculatecartprice(200, 399, 650));
+
+const user = {
+    username: "Eleus",
+    price: 199
+}
+
+function Handleobject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+    
+}
+// Handleobject(user)
+Handleobject({
+    username: "Bot",
+    price: 229
+})
+
+const newArray = [100, 200, 300, 400]
+
+function returnsecondvalue(getvalue){
+    return getvalue[1]
+}
+// console.log(returnsecondvalue(newArray));
+console.log(returnsecondvalue([500, 400, 300, 1000]));
 
