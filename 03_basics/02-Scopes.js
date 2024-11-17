6a4cc1007be913c8732f2a3a94ad77b0836ed9c6
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-17 08:09:16 +0000

    Scopes

--- 03_basics/02-Scopes.js
+++ 03_basics/02-Scopes.js
@@ -4,7 +4,7 @@ if (true) {
     let a = 10
     const b = 20
     var c = 30
-    console.log("Inner:", a);
+    // console.log("Inner:", a);
     
     
 }
@@ -16,4 +16,49 @@ if (true) {
 
 
 
+function one() {
+    const username = "Eleus"
+   function two() {
+    const website = "youtube"
+    console.log(username);
+     }
 
+    // console.log(website);
+
+    two()
+    }
+
+// one()
+
+
+if (true) {
+    const username = "Charlie"
+    if (username === "Charlie") {
+        const website = " Youtube"
+    // console.log(username + website);
+    
+    }
+    // console.log(website);
+    
+    
+}
+// console.log(website);
+
+
+//+++++++++++++++  Interesting  +++++++++++++++??
+
+addone()
+function addone(sum) {
+    return sum + 1
+    
+}
+
+console.log(addone(2))
+
+
+// addtwo()  *** Cannot access before ***
+const addtwo = function(num){
+    return num + 2
+}
+
+console.log(addtwo(6))
