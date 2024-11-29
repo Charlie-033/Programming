9c3161eddde7d9f809db2e4c5b85a5c575dd974e
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-29 15:43:25 +0000

    control_flow

---
+++ 04_Control_flow/one.js
@@ -0,0 +1,37 @@
+//If
+
+const userloggedIn = true
+const tempareture = 17
+
+if (tempareture < 20) {
+    // console.log('less than 20');
+    } else {
+        // console.log('greater than 20');
+        
+    }
+
+
+// > , < , >= , <= , != , == , ===    
+
+// const score = 200
+
+// if (score>199) {
+//     let power = 'fly'
+//     console.log(`user power = ${power}`);
+//     }
+
+// console.log(`user power = ${power}`);    
+
+
+const balance = 1000
+
+// if (balance>500) console.log('test');
+
+if (balance<500) {
+   console.log('less than 500');
+    } else if (balance<800) {
+       console.log('less than 750');
+        } else if (balance<1200) {
+            console.log('less than 1200');
+            }
+
