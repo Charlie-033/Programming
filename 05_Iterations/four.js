ca117662eef8954b3b0b3116d8056e2d3e5eee8f
Charlie-033 <ahmedeleus1@gmail.com>
2024-11-30 15:29:32 +0000

    loops

---
+++ 05_Iterations/four.js
@@ -0,0 +1,14 @@
+const Myobject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    swift: 'swift by apple'
+}
+for (const key in Myobject) {
+   //console.log(`${key} shortcurt is for ${Myobject[key]}`) 
+}
+
+const Arr = [ 'js', 'cpp', 'java', 'py']
+for (const key in Arr) {
+   console.log(Arr[key])
+}
