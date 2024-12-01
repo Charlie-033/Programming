7ee40b81764d04c6d7aafd34b3d757326f372794
Charlie-033 <ahmedeleus1@gmail.com>
2024-12-01 03:53:03 +0000

    loops

--- 05_Iterations/four.js
+++ 05_Iterations/four.js
@@ -10,5 +10,15 @@ for (const key in Myobject) {
 
 const Arr = [ 'js', 'cpp', 'java', 'py']
 for (const key in Arr) {
-   console.log(Arr[key])
+   //console.log(Arr[key])
+}
+
+const map = new Map()
+map.set('BD', 'Bangladesh')
+map.set('IN', 'India')
+map.set('FR', 'France')
+map.set('FR', 'France')
+
+for (const key in map) {
+  console.log(key)
 }
