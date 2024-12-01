675ee306247309264497c9aa6fbcc1869e863d83
Charlie-033 <ahmedeleus1@gmail.com>
2024-12-01 06:20:10 +0000

    filter

--- 05_Iterations/six.js
+++ 05_Iterations/six.js
@@ -20,19 +20,24 @@ Numbers.forEach( (num) => {
     }
 })
 
-console.log(newNum)
+//console.log(newNum)
 
 
 const Books = [
     {title: 'Book One', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Two', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Three', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Four', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Five', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Six', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Seven', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Eight', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Nine', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book Ten', genre: 'Science', publish:'1989' , edition: ' 2001'},
-    {title: 'Book One', genre: 'Science', publish:'1989' , edition: ' 2001'}
+    {title: 'Book Two', genre: 'Fiction', publish:'1984' , edition: ' 1999'},
+    {title: 'Book Three', genre: 'History', publish:'1999' , edition: ' 2009'},
+    {title: 'Book Four', genre: 'Novel', publish:'1981' , edition: ' 2000'},
+    {title: 'Book Five', genre: 'Science', publish:'1988' , edition: ' 2006'},
+    {title: 'Book Six', genre: 'Novel', publish:'1987' , edition: ' 2005'},
+    {title: 'Book Seven', genre: 'Fiction', publish:'1995' , edition: ' 2001'},
+    {title: 'Book Eight', genre: 'Poetry', publish:'2003' , edition: ' 2009'},
+    {title: 'Book Nine', genre: 'Science', publish:'2007' , edition: ' 2016'},
+    {title: 'Book Ten', genre: 'History', publish:'2012' , edition: ' 2018'}
 ]
+
+const userbooks = Books.filter( (book) => book.genre === 'History' )
+const userbook = Books.filter ( (book) => { 
+    return book.publish> 2000 && book.genre === 'Poetry'
+} )
+console.log(userbook)
